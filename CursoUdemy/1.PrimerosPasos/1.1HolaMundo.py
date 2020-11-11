91ad2fde4e1f325aa9f7dec0441c273f9ee5cc65
unknown <juanpablo.isabella@gmail.com>
2020-11-10 22:47:03 -0300

    primer clase

---
+++ CursoUdemy/1.PrimerosPasos/1.1HolaMundo.py
@@ -0,0 +1,6 @@
+#Escribir un "Hola Mundo"
+print("Hola Mundo") 
+print('Hola Mundo')
+print('Este es un curso de Python\nBienvenido')
+print('tema1\ttema2\ttema3')
+print('\th\te\tl\tl\to\nW\no\nr\nl\nd')
