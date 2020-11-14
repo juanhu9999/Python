0bc5537260760cdd0d5a9cc169382cdc8ad064c3
unknown <juanpablo.isabella@gmail.com>
2020-11-14 13:45:58 -0300

    primeras clases

--- CursoUdemy/1.PrimerosPasos/1.4OperadoresAritmeticos.py
+++ CursoUdemy/1.PrimerosPasos/1.4OperadoresAritmeticos.py
@@ -7,8 +7,14 @@ suma = Num1 + Num2
 resta = Num1 - Num2
 multi =  Num1 * Num2
 div = Num1 / Num2
+expo = Num1 ** Num2
+divint = Num1 // Num2
+modulo = Num1 % Num2
 
 print(suma)
 print(resta)
 print(multi)
 print(div)
+print(expo)
+print(divint)
+print(modulo)
---
+++ CursoUdemy/1.PrimerosPasos/1.5Jerarquia.py
@@ -0,0 +1,27 @@
+#Jerarquia de operaciones
+
+print((15+8)*10**2)
+
+#Ejercicio 1 Realizar una operación aritmetica
+
+print(pow(10,2,9))
+print(pow((3+2)/(2*5),2))
+
+
+Suma1 = (3+2)/(2*5)
+
+Suma2 = 2
+
+resultado = Suma1 ** Suma2
+
+print(resultado)
+
+#Jugueteria calcular el peso del pedido
+
+PayasoPesoGramos = 112
+
+MuñecaPesoGramos = 75
+
+PedidoCliente = PayasoPesoGramos * 23 + MuñecaPesoGramos * 54
+
+print(PedidoCliente)
