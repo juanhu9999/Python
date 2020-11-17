7b099ad992e543aa21d1ba28001a39d96b860ab3
unknown <juanpablo.isabella@gmail.com>
2020-11-17 00:11:50 -0300

    cadenas de texto

---
+++ CursoUdemy/1.PrimerosPasos/1.6Cadenas.py
@@ -0,0 +1,43 @@
+#Cadenas
+
+a = "uno"
+b = "dos"
+cadena = "Curso de Python"
+
+print(cadena[:3])
+
+print(cadena[3:])
+
+print(cadena[0:3])
+
+print(cadena[3])
+
+print(a + b)
+
+print(len(cadena))
+
+print(cadena.upper())
+
+print(cadena.lower())
+
+#Ejercicio
+
+cadena1 = "Ejercicio de Python"
+
+print(cadena1[0:2])
+
+print(cadena1[16:])
+
+print(cadena1[-3:])
+
+print(cadena1[::2])
+
+print(cadena1[::-1])
+
+print(cadena+cadena[::-1])
+
+cadena3 = "Separado"
+
+coma = ","
+
+print(','.join(cadena3))
