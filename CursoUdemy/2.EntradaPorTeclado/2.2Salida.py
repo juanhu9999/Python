776ea214ed9312cbe1eef29e91f0f2387c51153b
unknown <juanpablo.isabella@gmail.com>
2020-11-24 14:54:28 -0300

    Segunda parte clases

---
+++ CursoUdemy/2.EntradaPorTeclado/2.2Salida.py
@@ -0,0 +1,5 @@
+nombre = input("Ingresa tu nombre: ")
+edad = int(input("Ingresa tu edad: "))
+direccion = input("Ingresa tu dirección: ")
+
+print("Tu nombre es: {}, tu edad es: {} y vives en {}".format(nombre, edad, direccion))
