9283dbb5db77e347e92b54f934be24eadc1ec4cf
unknown <juanpablo.isabella@gmail.com>
2020-11-27 15:28:58 -0300

    tercera parte

---
+++ CursoUdemy/3.Condicionales/3.1OperadoresRelacionales.py
@@ -0,0 +1,12 @@
+num1 = 50
+num2 = 10
+num3 = 100
+num4 = 100
+
+print(num1 < num2)
+print(num1 > num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num3 >= num4)
+print(num1 != num2)
+print(num3 != num4)
---
+++ CursoUdemy/3.Condicionales/3.2OperadoresLogicos.py
@@ -0,0 +1,9 @@
+a = 50
+b = 150
+
+print( a > b or a < b)
+print(a > b or b > a)
+print(a == b or a >= b)
+print(a > b and a < b)
+print(a != b and b > a)
+print(not a < b)
