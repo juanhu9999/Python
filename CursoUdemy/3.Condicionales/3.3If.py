777074cb0eec21d46954f7162eb245feee35635d
unknown <juanpablo.isabella@gmail.com>
2020-11-30 12:32:00 -0300

    tercera parte clases

---
+++ CursoUdemy/3.Condicionales/3.3If.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingrese su numero: "))
+num2 = int(input("Ingrese el siguiente numero: "))
+
+if num1 > num2:
+    print("El numero uno, es mayor al numero dos.")
+else:
+    print("El numero dos, es mayor al numero uno.")
