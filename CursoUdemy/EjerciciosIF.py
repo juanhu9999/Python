b596383e6ba249f498fd72b09370e1de8a2aae99
unknown <juanpablo.isabella@gmail.com>
2020-11-30 12:31:01 -0300

    Tareas

---
+++ CursoUdemy/EjerciciosIF.py
@@ -0,0 +1,22 @@
+"""Ejercicio 1
+
+Crear un programa que pida al usuario una letra, y si es vocal, muestre el mensaje "Es vocal". Sino, decirle al usuario que no es vocal
+
+
+
+Ejercicio 2
+
+Escribir un programa que, dado un número entero, muestre su valor absoluto. Nota: para los números positivos su valor absoluto es igual al número
+ (el valor absoluto de 52 es 52), mientras que, para los negativos, su valor absoluto es el número multiplicado por -1 (el valor absoluto de -52 es 52)."""
+
+
+#Ejercicio1
+
+Letra = input("ingresa una letra: ")
+
+if Letra in "aeiouAEIOU" :
+    print("Es vocal")
+else:
+    print("no es vocal")
+
+#Ejercicio2
