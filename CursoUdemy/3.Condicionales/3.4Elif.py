7c78e45b3aa50681a1b2f40d97e0d56c266bac8e
unknown <juanpablo.isabella@gmail.com>
2020-12-01 12:19:57 -0300

    tercera parte

---
+++ CursoUdemy/3.Condicionales/3.4Elif.py
@@ -0,0 +1,17 @@
+
+
+num = int(input("Ingrese un numero del 1 al 5: "))
+
+if num == 1:
+    print("Tu numero es el uno")
+elif num == 2:
+    print("Tu numero es el dos")
+elif num == 3:
+    print("Tu numero es el tres")
+elif num == 4:
+    print("Tu numero es el cuatro")
+elif num == 5:
+    print("Tu numero es el cinco")
+else:
+    print("Has ingresado un numero incorrecto")
+
