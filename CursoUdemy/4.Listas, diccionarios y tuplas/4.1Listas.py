ad952a28b286483442b124c8b9a4057c94878441
unknown <juanpablo.isabella@gmail.com>
2020-12-03 13:14:02 -0300

    cuarta parte

---
+++ CursoUdemy/4.Listas, diccionarios y tuplas/4.1Listas.py
@@ -0,0 +1,18 @@
+Lista = ["Tomate", "Cebolla", "Huevos", "Arroz"]
+Lista2 = [1, 2, 3, 4, 5]
+Lista3 = [6, 7, 8, 9, 10]
+
+print(Lista)
+print(Lista2)
+print(Lista3)
+print(Lista[1])
+print(Lista2[4])
+print(Lista3[0])
+print("Arroz" in Lista)
+print(15 in Lista)
+
+print(Lista2[:3])
+
+fruta = "banana"
+
+print(fruta[0:5])
