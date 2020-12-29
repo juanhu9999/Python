487a6ecbf4dcf88bc6a34faf2634ff40901457c9
unknown <juanpablo.isabella@gmail.com>
2020-12-28 21:46:22 -0300

    cuarta parte

---
+++ CursoUdemy/4.Listas, diccionarios y tuplas/4.4Diccionario.py
@@ -0,0 +1,11 @@
+diccionario = {1: "Jorge Perez", 2: "Alberto lopez", 3: "Carlitos Gimenez"}
+
+print(type(diccionario))
+print(diccionario)
+print(diccionario[3])
+print(diccionario[1])
+print(2 in diccionario)
+
+value = diccionario.values()
+
+print("Jorge Perez" in value)
